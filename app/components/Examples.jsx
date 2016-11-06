821ae33e4b33012e9ab3410472d47c53c09c3d15
Mike <pardamike@gmail.com>
2016-11-06 13:38:44 -0500

    small fix

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component</h3>
-    <h4>Welcome to the examples page!</h4>
+    <div>
+      <h3>Examples Component</h3>
+      <h4>Welcome to the examples page!</h4>
+    </div>
   )
 };
 
