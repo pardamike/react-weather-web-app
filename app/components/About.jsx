163c42753a87b6f2730c8dbad0c2dec61bd1f294
Mike <pardamike@gmail.com>
2016-11-06 22:50:03 -0500

    style about page and added links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,21 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a simple weather application built using React and the free Open Weather Map API:</p>
+      <ul>
+        <li>
+          <a target="_blank" href="https://facebook.github.io/react">React JS</a>
+        </li>
+        <li>
+          <a target="_blank" href="http://openweathermap.org">Open Weather Map</a>
+        </li>
+        <li>
+          <a target="_blank" href="https://github.com/pardamike/react-weather-web-app">View this app on Github</a>
+        </li>
+      </ul>
+    </div>
   )
 };
 
